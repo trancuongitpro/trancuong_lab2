3e9f6c7a68c3aa52f1e013a4f8764df8b26eb18c
trancuongitpro@gmail.com <trancuongitpro@gmail.com>
2021-06-04 22:36:34 +0700

    Update lab2

---
+++ Exercise2/ObjectArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ObjectArray
+{
+    class Employee
+    {
+        private int empID;
+
+        public Employee(int empID)
+        {
+            this.empID = empID;
+        }
+
+        public override string ToString()
+        {
+            return empID.ToString();
+        }
+    }
+    
+    internal class ObjectArray
+    {
+        public void Run()
+        {
+            int[] intArray;
+            Employee[] empArray;
+            intArray = new int[5];
+            empArray = new Employee[3];
+
+            for (int i = 0; i < empArray.Length; i++)
+            {
+                empArray[i] = new Employee(i + 5);
+            }
+
+            Console.WriteLine("The int array...");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i].ToString());
+            }
+
+            Console.WriteLine("\nThe employee array...");
+            for (int i = 0; i < empArray.Length; i++)
+            {
+                Console.WriteLine(empArray[i].ToString());
+            }
+        }
+        public static void Main(string[] args)
+        {
+            ObjectArray arr = new ObjectArray();
+            arr.Run();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise4/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApplication8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
